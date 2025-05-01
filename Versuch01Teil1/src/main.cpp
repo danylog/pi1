2172d7050cfbd9de4b5cf68e915e9ebeffa62894
Danylo Galytskyy <work.danylo.galytskyy@gmail.com>
2025-05-01 17:50:36 +0200

    1.6.1.1

--- Versuch01Teil1/src/main.cpp
+++ Versuch01Teil1/src/main.cpp
@@ -1,11 +1,17 @@
 #include <stdio.h>
 #include <iostream>
-int iErste, iZweite;
+int iErste, iZweite, iSumme, iQuotient;
 
 int main(){
 	std::cout << "geben Sie den ersten Nummer ein"  << std::endl;
 	std::cin >> iErste;
 	std::cout << "geben Sie den zweiten Nummer ein " << std::endl;
 	std::cin >> iZweite;
+
+	iSumme = iErste + iZweite;
+	iQuotient = iErste/iZweite;
+
+	std::cout << "Summe: " << iSumme << std::endl;
+	std::cout << "Division: " << iQuotient << std::endl;
 }
 
