9212f93ad9cea0299ef0b3ca30d17dac6c5a0ba9
Danylo Galytskyy <work.danylo.galytskyy@gmail.com>
2025-05-01 17:10:35 +0200

    cin cout works

---
+++ Versuch01Teil1/src/main.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <iostream>
+int iErste, iZweite;
+
+int main(){
+	std::cout << "geben Sie den ersten Nummer ein"  << std::endl;
+	std::cin >> iErste;
+	std::cout << "geben Sie den zweiten Nummer ein " << std::endl;
+	std::cin >> iZweite;
+}
+
