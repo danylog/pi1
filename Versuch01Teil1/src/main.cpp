aaa131770ccb2c9c831f56a0cecdb6051d91cdca
Danylo Galytskyy <work.danylo.galytskyy@gmail.com>
2025-05-01 17:57:43 +0200

    1.6.1.3

--- Versuch01Teil1/src/main.cpp
+++ Versuch01Teil1/src/main.cpp
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <iostream>
 int iErste, iZweite, iSumme, iQuotient;
+double dSummeCast, dQuotientCast;
 
 int main(){
 	std::cout << "geben Sie den ersten Nummer ein"  << std::endl;
@@ -8,10 +9,19 @@ int main(){
 	std::cout << "geben Sie den zweiten Nummer ein " << std::endl;
 	std::cin >> iZweite;
 
+	//integers
 	iSumme = iErste + iZweite;
 	iQuotient = iErste/iZweite;
 
 	std::cout << "Summe: " << iSumme << std::endl;
 	std::cout << "Division: " << iQuotient << std::endl;
+
+	//casted
+	dSummeCast = double(iErste) + double(iZweite);
+	dQuotientCast = double(iErste)/double(iZweite);
+
+
+	std::cout << "Summe (cast): " << dSummeCast << std::endl;
+	std::cout << "Division (cast): " << dQuotientCast << std::endl;
 }
 
